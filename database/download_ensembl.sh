26d1d46fc23ad90a34beb6a03de8f26c7bbb3c99
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2015-09-24 17:12:16 +0200

    let the user define the specie, release and sub release

--- database/download_ensembl.sh
+++ database/download_ensembl.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+specie=$1
+release=$2
+subrelease=$3
+
+if [ -z "$specie" ]; then
+  echo "Please specify a specie, i.e. homo_sapiens"
+  exit 1
+fi
+
+if [ -z "$release" ]; then
+  echo "Please specify a release, i.e. 81"
+  exit 1
+fi
+
+if [ -z "$subrelease" ]; then
+  echo "Please specify the sub release, i.e. 38"
+fi
 # http://www.ensembl.org/info/docs/webcode/mirror/install/ensembl-data.html
 # To install the Ensembl Data:
 
@@ -35,62 +54,71 @@ function if_something_goes_wrong {
     local status=$1
     local message=$2
     if [ $status -ne 0 ]; then
-        echo message >&2
+        echo $message >&2
         exit $status
     fi
 }
 
 
 # dbname
+# mysql_host_ip
 function clone_ensembl_db {
 
   local dbname=$1
+  local dbhost=$2
   local status=0
+  local sourcedir=/ftp
 
-  if [ ! -e /ftp/${dbname}.downloaded ]; then
-    rsync -avP rsync://ftp.ensembl.org/ensembl/pub/current_embl/${dbname} /ftp/ &&\
-    touch ${dbname}.downloaded
+  if [ ! -e ${sourcedir}/${dbname}.downloaded ]; then
+    rsync -avP rsync://ftp.ensembl.org/ensembl/pub/current_mysql/${dbname} ${sourcedir}/ &&\
+    touch ${sourcedir}/${dbname}.downloaded
 
-    if_something_goes_wrong $? "Database ${dbname} can not be dowloaded correctly."
+    if_something_goes_wrong $? "Database ${dbname} can not be dowloaded correctly. Please check your internet connection, the name of the specie or the release number"
+  fi 
 
-  else
-
-    if [ ! -e /ftp/${dbname}.dbcreated ]; then
-      create database homo_sapiens_core_81_38 &&\
-      touch /ftp/${dbname}.dbcreated
+  if [ ! -e ${sourcedir}/${dbname}.dbcreated ]; then
+      mysql -u mysqldba -h ${dbhost} \
+          -e "CREATE DATABASE IF NOT EXISTS \`${dbname}\` DEFAULT CHARACTER SET \`utf8\` COLLATE \`utf8_unicode_ci\`;" &&\
+      touch ${sourcedir}/${dbname}.dbcreated
       if_something_goes_wrong $? "Database ${dbname} can not be created into MySQL."
-    fi
+  fi
 
-    if [ ! -e /ftp/${dbname}.schemaloaded ]; then
-      cd /ftp/${dbname} &&\
+  if [ ! -e ${sourcedir}/${dbname}.schemaloaded ]; then
+      cd ${sourcedir}/${dbname} &&\
       gunzip ${dbname}.sql.gz &&\
-      mysql -u mysqldba ${dbname} < ${dbname}.sql &&\
+      mysql -h ${dbhost} -u mysqldba ${dbname} < ${dbname}.sql &&\
       gzip ${dbname}.sql &&\
-      touch /ftp/${dbname}.schemaloaded
+      touch ${sourcedir}/${dbname}.schemaloaded
       if_something_goes_wrong $? "Database ${dbname} schema can not be loaded into MySQL."
-    fi
+  fi
 
-    if [ ! -e /ftp/${dbname}.dbloaded ]; then
-      cd /ftp/${dbname} &&\
-      mysqlimport -u mysqldba --fields_escaped_by=\\ ${dbname} -L *.txt &&\
-      touch /ftp/${dbname}.dbloaded
-      if_something_goes_wrong $? "Database ${dbname} can not be loaded into MySQL."
-    fi
+  if [ ! -e ${sourcedir}/${dbname}.dbloaded ]; then
+      are_there_txts=$(ls ${sourcedir}/${dbname}/*.txt.gz &>/dev/null)
+      if [ $? -eq 0 ]; then
+        cd ${sourcedir}/${dbname} &&\
+        gunzip *.txt.gz &&\
+        mysqlimport -h ${dbhost} -u mysqldba --fields_escaped_by=\\ ${dbname} -L *.txt &&\
+        gzip *.txt &&\
+        touch ${sourcedir}/${dbname}.dbloaded
+        if_something_goes_wrong $? "Database ${dbname} can not be loaded into MySQL."
+      else
+        echo "Database ${dbname} has no data to be loaded"
+      fi
   fi
 
 }
 
 
 # Required all R expect where specified
-for database in ensembl_ontology_81 ensembl_website_81 ensembl_accounts do
-  clone_ensembl_db ${database}
+for database in ensembl_ontology_${release} ensembl_website_${release} ensembl_accounts; do
+  clone_ensembl_db ${database} ${ENSEMBL_DB_PORT_3306_TCP_ADDR}
 done
 
 
 # By Specie
-# for database in homo_sapiens_core_81_38 homo_sapiens_funcgen_81_38 homo_sapiens_otherfeatures_81_38 homo_sapiens_variation_81_38 do
-#   clone_ensembl_db ${database}
-# done
+ for database in ${specie}_core_${release}_${subrelease} ${specie}_funcgen_${release}_${subrelease} ${specie}_otherfeatures_${release}_${subrelease} ${specie}_variation_${release}_${subrelease}; do
+  clone_ensembl_db ${database} ${ENSEMBL_DB_PORT_3306_TCP_ADDR}
+ done
 
 
 # ensembl_go_81
